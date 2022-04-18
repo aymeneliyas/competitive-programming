a55c8aeec38dc476ed91b242f83e4e9892308ebe
Aymen eliyas <aymeneliyas8@gmail.com>
2022-04-18 01:01:56 -0700

    fizz buzz solution

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+    	List<String>answer = new ArrayList<String>();
+        for (int i = 1; i < n+1; i++)
+        {
+        	if (i % 3 == 0 && i % 5 == 0)
+        		answer.add("FizzBuzz");
+        	else if (i % 3 == 0 && i % 5 != 0)
+        		answer.add("Fizz");
+        	else if (i % 3 != 0 && i % 5 == 0)
+        		answer.add("Buzz");
+        	else
+        		answer.add(Integer.toString(i));
+        }
+        return answer;
+    }
+}public class FizzBuzz {
+    
+}
